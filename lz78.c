b266b893c8d5bb7fb3bcf0eb0bdd5a8a0c6cfcaf
vince.pii <vinc.pii@gmail.com>
2010-04-22 23:00:23 +0200

    Funziona hash

--- lz78.c
+++ lz78.c
@@ -1,28 +1,99 @@
 #include "lz78.h"
 
 struct node* dict_init(){
-	struct node* dictionary;
-	unsigned int double_size;
-	int i = 0;
+	struct node* ht;
+	struct lz78_c* dict;
+	unsigned int size;
+	unsigned int i = 0;
 
-	//2*DICT_SIZE, ovvero 2^21
-	double_size = DICT_SIZE << 1;
+	//DICT_SIZE ovvero 2^21
+	size = DICT_SIZE;
 	//9 byte * 2^21 = 18MB
-	dictionary = (struct node*)malloc(double_size * sizeof(struct node));
-	if (dictionary == NULL){
-		sys_err("dict_init: error allocating dictionary");
+	ht = (struct node*)malloc(size * sizeof(struct node));
+	if (ht == NULL){
+		sys_err("dict_init: error allocating hash table");
 	}
-	bzero(dictionary, double_size);
-	for (i = 0; i < 256; i++) {
-		dictionary[i].character = (char)i;
-		dictionary[i].code = i;
-		dictionary[i].parent_code = 0;
-	}
-	for (; i < double_size; i++) {
-		dictionary[i].character = 0;
-		dictionary[i].code = -1;
-		dictionary[i].parent_code = 0;
-	}
+	bzero(ht, size);
 
-	return dictionary;
+	dict = (struct lz78_c*)malloc(sizeof(struct lz78_c));
+	if (dict == NULL){
+		sys_err("dict_init: error allocating lz78_c struct");
+	}
+	bzero(dict, sizeof(struct lz78_c));
+
+	//TODO: eliminare le codifiche singole dal dizionario
+	for (i = 0; i < 256; i++) {
+		ht[i].character = (char)i;
+		ht[i].code = i;
+		ht[i].parent_code = ROOT_CODE;
+	}
+	//inefficiente: si dovrebbe sfruttare la bzero per inizializzare
+	/*
+	 * TODO: improvement: eliminare dalla hash le codifiche dei caratteri
+	 * singoli (codifiche ASCII < 256): quando vengono letti, si conosce
+	 * implicitamente che parent_code = 0, characheter = c, code = int(c).
+	 * Di conseguenza devono essere cambiate le define: EMPTY_NODE 0, per avere
+	 * inizializzazione fatta solo con la bzero, ROOT_CODE 1, etc.
+	 * */
+	for (; i < size; i++) {
+		ht[i].character = 0;
+		ht[i].code = EMPTY_NODE_CODE;
+		ht[i].parent_code = 0;
+	}
+	dict->dict = ht;
+	dict->hash_size = FIRST_CODE - 1;
+	dict->d_next = FIRST_CODE;
+	dict->nbits = ceil_log2(dict->hash_size);
+	return ht;
+}
+
+//funzione ripresa dal "The Data Compression Book"
+unsigned int find_child_node(unsigned int parent_code,
+		unsigned int child_char,
+		struct lz78_c* comp)
+{
+	unsigned int index;
+	int offset;
+
+	/* Con lo shift al max si ottiene 255 << 13 = 2088960 (DICT_SIZE 2097143)
+	 * Segue xor col codice del padre ==> sicuramente index è nel range
+	 * */
+	index = (child_char << ( BITS - 8 )) ^ parent_code;
+	if (index == 0) {
+		offset = 1;
+	}
+	else {
+		offset = DICT_SIZE - index;
+	}
+	for ( ; ; ) {
+		if (comp->dict[index].code == EMPTY_NODE_CODE) {
+			//empty node
+			return((unsigned int)index);
+		}
+		if (comp->dict[index].parent_code == parent_code &&
+				comp->dict[index].character == (char)child_char) {
+			//match
+			return(index);
+		}
+		if (index >= offset) {
+			index -= offset;
+		}
+		else {
+			index += DICT_SIZE - offset;
+		}
+	}
+}
+
+unsigned int ceil_log2(unsigned int x)
+{
+	unsigned int cont = 0;
+
+	if (x == 0) {
+		user_err("ceil_log2: cannot calculate log(0)");
+	}
+	while (x > 0) {
+		x = x >> 1;
+		cont++;
+	}
+	return cont;
 }
--- lz78.h
+++ lz78.h
@@ -1,13 +1,79 @@
 #include "bit_io.h"
 #include "utility.h"
 
-//DICT_SIZE: 2^20
-#define DICT_SIZE (1 << 20)
+#ifndef __LZ78_H__
+#define __LZ78_H__
+
+//DICT_SIZE: prime number close to 2 * 2^20 = 2097152
+//TODO: la dimensione del dizionario deve poter essere specificata dall'utente
+//come calcolare numero primo + piccolo della dim passata?
+#define DICT_SIZE 2097143
+
+//max number of bit for codes
+#define BITS 21
+
+//root node's code
+#define ROOT_CODE 256
+
+//code value for an empty node
+#define EMPTY_NODE_CODE 257
+
+//comunicazione al decompressore della dimensione del dizionario
+//#define DICT_LENGTH_CODE
+//comunicazione di fine file
+//#define EOF_CODE
+
+//first empty node when the hash table is first created
+#define FIRST_CODE 258
 
 struct node {
-	int code;
+	unsigned int code;
 	char character;
-	int parent_code;
+	unsigned int parent_code;
+};
+
+struct lz78_c {
+	struct node* dict;
+/*
+ * nodo corrente fino al quale si ha avuto match nel cercare una stringa
+ * nell'albero. E' ciò che viene ritornato dalla find_child_node, e la codifica
+ * da emettere sarà dict[cur_node].code (in bit). N.B.: la find child node deve
+ * essere richiamata ogni volta, per vedere se il nodo corrente ha un figlio
+ * con un certo carattere e quindi per prolungare il più possibile il match.
+ *
+ * Nodo ritornato dalla find_child_node, che viene chiamata per cercare se
+ * un certo nodo (padre), ha un figlio con un certo carattere.
+ * La find_child_node quindi deve essere chiamata più volte, finché si continua
+ * a trovare match della sequenza. Quando ritorna un nodo vuoto, si emette
+ * la codifica del nodo precedente, che corrisponde alla codifica della
+ * sequenza che ha prodotto il match più lungo.
+ * */
+	unsigned int cur_node;
+	/*
+	 * Next location where to insert a new node in the hash table
+	 * */
+	unsigned int d_next;
+
+	/**
+	 * Hash table size
+	 */
+	unsigned int hash_size;
+
+	/**
+	 * Numero di bit delle codifiche, deve valere ceil(log2(hash_size))
+	 */
+	unsigned int nbits;
 };
 
 struct node* dict_init();
+unsigned int find_child_node(unsigned int parent_code,
+		unsigned int child_char,
+		struct lz78_c* comp);
+
+/**
+ * Utility function which calculates the ceiling of a base 2 log of an integer
+ */
+unsigned int ceil_log2(unsigned int x);
+
+#endif
+
